85d72ffdbd5482e7340e61ee0c4697200657c5be
Fabio Fogliarini Brolesi <brolesi@gmail.com>
2023-05-17 07:50:50 -0300

    Cookiecutter structure and initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The capstone project of deep learning course at UNICAMP',
+    author='Fabio Fogliarini Brolesi',
+    license='MIT',
+)
